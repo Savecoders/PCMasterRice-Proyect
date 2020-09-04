844f95898b4c5016580ec8d86a486d525c1abb6d
pablo-pincay <pablo.pincay.alvarez@hotmail.com>
2020-09-04 00:04:58 -0500

    PrimeraPrueba2

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -4,3 +4,46 @@ const nav = document.querySelector('.navbar ul');
 check.addEventListener('click', () => {
 	nav.classList.toggle('anime');
 });
+
+/* import.css*/
+$(document).ready(function () {
+	$('#slider').fadeIn('slow').delay(200);
+	$('#sticker').sticky({ topSpacing: 0 });
+});
+
+$(document).ready(function () {
+	$('a').on('click', function (event) {
+		if (this.hash !== '') {
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate(
+				{
+					scrollTop: $(hash).offset().top,
+				},
+				2000,
+				function () {
+					window.location.hash = hash;
+				}
+			);
+		}
+	});
+});
+
+//FIXED MENU COLORS
+$(document).ready(function () {
+	$(window).trigger('scroll');
+	$(window).bind('scroll', function () {
+		var pixels = 50; //pixeles abajo
+		if ($(window).scrollTop() > pixels) {
+			$('.nav').addClass('fixed');
+			$('.menu a').css({ color: '#354757' });
+			$('.logo').css({ color: '#354757' });
+		} else {
+			$('.nav').removeClass('fixed');
+			$('.menu a').css({ color: '#BABCBD' });
+			$('.logo').css({ color: '#BABCBD' });
+		}
+	});
+});
